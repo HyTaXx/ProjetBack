a51a145ec6ca7ae7cb9bd5a4de7953f9181bc23c
Armand Nobre <nobrearmand@gmail.com>
2023-01-06 14:47:33 +0100

    fix pages + recherche

--- public/js/search.js
+++ public/js/search.js
@@ -36,7 +36,57 @@ function getValue(result){
 }
 
 function displayMovieSearched(a){
+                let balisea = document.createElement('a')
+
+                let iWatch = document.createElement('i')
+                iWatch.classList.add("fa-solid")
+                iWatch.classList.add("fa-eye")
+                balisea.appendChild(iWatch)
+                balisea.href = 'singlemovie.php?id=' + a.id
+
+                
+                let iSeen = document.createElement("i")
+                iSeen.classList.add("fa-solid")
+                iSeen.classList.add("fa-user-check")
+                let button2 = document.createElement('button')
+                button2.appendChild(iSeen)
+                button2.type = 'submit'
+
+                let like = document.createElement('form')
+                let iLike = document.createElement("i")
+                like.method = 'POST'
+                let liked = document.createElement('input')
+                liked.name = 'liked'
+                liked.value = a.id
+                liked.type = 'hidden'
+                let buttonx = document.createElement('button')
+                iLike.classList.add("fa-regular")
+                iLike.classList.add("fa-thumbs-up")
+                buttonx.appendChild(iLike)
+                buttonx.type = 'submit'
+                like.appendChild(liked)
+                like.appendChild(buttonx)
+                
+                let form = document.createElement('form')
+                form.method = 'GET'
+                let addtoalbum = document.createElement('input')
+                addtoalbum.type = 'hidden'
+                addtoalbum.name = 'filmid'
+                addtoalbum.value = a.id
+                let button = document.createElement('button')
+                let iAdd = document.createElement("i")
+                iAdd.classList.add("fa-solid")
+                iAdd.classList.add("fa-circle-plus")
+                button.appendChild(iAdd)
+                button.type = 'submit'
+                form.appendChild(addtoalbum)
+                form.appendChild(button)
+
                 let stockbtn = document.createElement('div')
+                stockbtn.appendChild(like)
+                stockbtn.appendChild(form)
+                stockbtn.appendChild(balisea)
+                stockbtn.appendChild(button2)
                 stockbtn.classList.add("flex")
                 stockbtn.classList.add("flex-row")
                 stockbtn.classList.add("gap-5")
@@ -47,6 +97,7 @@ function displayMovieSearched(a){
                 newdiv.classList.add("flex-col")
                 newdiv.classList.add("w-[200px]")
                 let h2 = document.createElement("h2")
+                h2.classList.add("font-bold")
                 h2.innerHTML = a.title
                 let img = document.createElement('img')
                 img.src = 'https://image.tmdb.org/t/p/w500' + a.poster_path
